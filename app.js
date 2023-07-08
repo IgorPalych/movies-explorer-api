a7fb93863fdefb5c8f5eea27fec9576438fe9fcd
IgorPalych <mylnx@mail.ru>
2023-07-08 16:43:25 +0300

    feat: реализовал логи и безопасность приложения

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const { errors } = require('celebrate');
 
 const router = require('./routes');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { handleError } = require('./middlewares/handleError');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 app.use(express.json());
 
+app.use(helmet());
+
+app.use(limiter);
+
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
